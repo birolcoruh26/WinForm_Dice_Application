1e827fd7af909d7044076589dc727eccb3051c2e
103AKSAM_BİROL <103AKSAM_BROL@DESK6>
2021-12-30 21:17:29 +0300

    Add project files.

---
+++ ZarAtmaUygulamasıWinForm/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace ZarAtmaUygulamasıWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btn_ZarAt = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ZarAtmaUygulamasıWinForm.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(1049, 291);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(209, 153);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ZarAtmaUygulamasıWinForm.Properties.Resources._1;
+            this.pictureBox2.Location = new System.Drawing.Point(822, 291);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(209, 153);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "1.PNG");
+            this.ımageList1.Images.SetKeyName(1, "2.PNG");
+            this.ımageList1.Images.SetKeyName(2, "3.PNG");
+            this.ımageList1.Images.SetKeyName(3, "4.PNG");
+            this.ımageList1.Images.SetKeyName(4, "5.PNG");
+            this.ımageList1.Images.SetKeyName(5, "6.PNG");
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(918, 450);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 74);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Çıkış Yap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Showcard Gothic", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.SystemColors.MenuHighlight;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(250, 27);
+            this.dateTimePicker1.TabIndex = 4;
+            this.dateTimePicker1.Value = new System.DateTime(2009, 10, 29, 0, 0, 0, 0);
+            // 
+            // btn_ZarAt
+            // 
+            this.btn_ZarAt.Location = new System.Drawing.Point(918, 198);
+            this.btn_ZarAt.Name = "btn_ZarAt";
+            this.btn_ZarAt.Size = new System.Drawing.Size(209, 74);
+            this.btn_ZarAt.TabIndex = 5;
+            this.btn_ZarAt.Text = "Zar At";
+            this.btn_ZarAt.UseVisualStyleBackColor = true;
+            this.btn_ZarAt.Click += new System.EventHandler(this.btn_ZarAt_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(843, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(349, 27);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1208, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(961, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.Visible = false;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(91, 39);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 27);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "1";
+            this.textBox3.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1270, 536);
+            this.ControlBox = false;
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_ZarAt);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btn_ZarAt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ ZarAtmaUygulamasıWinForm/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZarAtmaUygulamasıWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.CadetBlue;
+            timer2.Interval = 1000;
+        }
+        int sayac = 0;
+        int zar1, zar2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+                Image myimage = new Bitmap(@"C:\Users\103AKSAM_BİROL\Source\Github\ZarAtmaUygulamas-WinForm-master\fotolar\ekremabisilah.jpg");
+                this.BackgroundImage = myimage;
+                DialogResult dr = MessageBox.Show(this, "kendi payının ... koymak istiyormusun?", "Title", MessageBoxButtons.YesNo);
+                if (dr == DialogResult.Yes)
+                {
+                myimage = new Bitmap(@"C:\Users\103AKSAM_BİROL\Source\Github\ZarAtmaUygulamas-WinForm-master\fotolar\EdohiUOWkAALk8Q.jpg");
+                //myimage = Image.FromFile(@"C:\Users\103AKSAM_BİROL\OneDrive\Masaüstü\a1.png");
+                this.BackgroundImage=myimage;
+                DialogResult ab = MessageBox.Show(this, "son kararın mı?", "Title", MessageBoxButtons.YesNo);
+                    if (ab == DialogResult.Yes)
+                    {
+                     this.Close();
+                    }
+                    else
+                    {
+                    MessageBox.Show("emin olunamadı");
+                    }
+                }
+                else
+                {
+                MessageBox.Show("koyulamadı");
+                }
+
+        }
+
+   
+    
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          
+
+            Random rnd = new Random();
+            zar1 = rnd.Next(1, 7);
+            zar2 = rnd.Next(1, 7);
+            switch (zar1)
+            {
+                case 1:
+                    pictureBox1.Image = ımageList1.Images[0];
+                        break;
+                case 2:
+                    pictureBox1.Image = ımageList1.Images[1];
+                    break;
+                case 3:
+                    pictureBox1.Image = ımageList1.Images[2];
+                    break;
+                case 4:
+                    pictureBox1.Image = ımageList1.Images[3];
+                    break;
+                case 5:
+                    pictureBox1.Image = ımageList1.Images[4];
+                    break;
+                case 6:
+                    pictureBox1.Image = ımageList1.Images[5];
+                    break;
+                default:
+                    break;
+            }
+            switch (zar2)
+            {
+                case 1:
+                    pictureBox2.Image = ımageList1.Images[0];
+                    break;
+                case 2:
+                    pictureBox2.Image = ımageList1.Images[1];
+                    break;
+                case 3:
+                    pictureBox2.Image = ımageList1.Images[2];
+                    break;
+                case 4:
+                    pictureBox2.Image = ımageList1.Images[3];
+                    break;
+                case 5:
+                    pictureBox2.Image = ımageList1.Images[4];
+                    break;
+                case 6:
+                    pictureBox2.Image = ımageList1.Images[5];
+                    break;
+            }
+            textBox2.Text = Convert.ToString(zar1 + zar2);
+            
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Visible = true;
+        }
+        int time = 1;
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            time++;
+            textBox3.Visible = true;
+            textBox3.Text = time.ToString();
+
+        }
+
+        private void btn_ZarAt_Click_1(object sender, EventArgs e)
+        {
+            sayac += 1;
+            timer2.Start();
+            timer1.Start();
+            label1.Visible = false;
+            textBox1.Visible = false;
+            for (int i = 0; i < 1000; i++)
+            {
+                if(sayac % 2 == 0)
+                {
+                timer1.Stop();
+                    if (zar1 + zar2 == 12)
+                    {
+                        label1.Visible = true;
+                        textBox1.Visible = true;
+                        label1.Text = "Düşeş";
+                        textBox1.Text = "Akıyo Bu Akşam Maşallah";
+                    }
+                    else if (zar1 +zar2 == 2)
+                    {
+                        textBox1.Visible = true;
+                        textBox1.Text = "Böyle Adama Pamuk Mu Tıkılır Be";
+                    }
+                }
+            }
+           
+
+
+
+        }
+    }
+}
